5f2e23ab717d6a01c0a62c4a2f939ae8ff120c49
James Brown <jamesbrown19921992@hotmail.com>
2026-08-09 21:59:34 +0100

    Bump versionCode to 2 for the ScrollBeat release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.scrollstop.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
